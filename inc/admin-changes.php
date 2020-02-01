b188092dd179d8e8242c8c0dd5ea460b69ee5279
Luisoncm <luisoncm@gmail.com>
2020-02-01 13:41:43 +0100

    Remove some nodes from WordPress admin bar.

--- inc/admin-changes.php
+++ inc/admin-changes.php
@@ -2,16 +2,17 @@
 /**
  * Core Functionality Plugin
  * 
+ * Custom Admin Menu changes
+ * 
  * @package    CoreFunctionality
  * @since      0.0.7
  * @license    GPL-2.0+
  */
 
- /*--------------------------------------------*
- * Custom Admin Menu changes
- *--------------------------------------------*/
-
-// Rename Posts to Articles in Menu
+/**
+ * Rename Posts to Articles in Menu
+ * 
+ */
 function change_post_menu_label() {
 	global $menu;
 	global $submenu;
@@ -20,3 +21,18 @@ function change_post_menu_label() {
 	$submenu['edit.php'][10][0] = 'Add New ';
 }
 add_action( 'admin_menu', 'change_post_menu_label' );
+
+
+/**
+ * Remove items from admin bar
+ * 
+ */ 
+add_action( 'admin_bar_menu', 'lcm_remove_nodes', 999 );
+function lcm_remove_nodes(){
+	
+	global $wp_admin_bar;
+
+	$wp_admin_bar->remove_node('wp-logo');
+	$wp_admin_bar->remove_node('comments');
+
+}
