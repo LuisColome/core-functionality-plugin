7713474524399919255d3c894b71a95b3c08c3b1
Luis Colomé <hello@luiscolome.com>
2023-06-15 16:50:16 +0200

    Update version

--- core-functionality.php
+++ core-functionality.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Core Functionality
  * GitHub Plugin URI: https://github.com/Luisoncm/Core-functionality-plugin
  * Description: <strong>DO NOT DISABLE!</strong> This contains all your site's core functionality so that it is theme independent.
- * Version: 1.0.2.1
+ * Version: 1.0.2.2
  * Author: Luis Colomé
  * Author URI: http://www.luiscolome.com
  *
